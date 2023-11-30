f0991df6114175810c81b06a5123805a8e2e0fd3
Martin Harris <1488244+nakomis@users.noreply.github.com>
2023-11-30 12:47:34 +0000

    194. Get List as Java Map

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,34 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
+
+        System.out.println(response.getBody());
+
         return null;
     }
 }
