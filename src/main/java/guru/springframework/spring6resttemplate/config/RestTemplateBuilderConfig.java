8c0de0c60417e7a0a0e650c03b5535513f588cc6
Martin Harris <1488244+nakomis@users.noreply.github.com>
2023-12-01 13:04:53 +0000

    222. HTTP Basic with RestTemplate

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6resttemplate.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,17 +9,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     private String rootUrl;
 
+    @Value("${com.nakomis.rest.template.username}")
+    private String username;
+
+    @Value("${com.nakomis.rest.template.password}")
+    private String password;
+
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
-
         assert rootUrl != null;
 
-        RestTemplateBuilder builder = configurer.configure(new RestTemplateBuilder());
+        RestTemplateBuilder builder = configurer
+                .configure(new RestTemplateBuilder())
+                .basicAuthentication(username, "hunter2");
+        
         DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory(rootUrl);
 
         return builder.uriTemplateHandler(uriBuilderFactory);
